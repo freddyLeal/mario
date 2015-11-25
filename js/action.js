a0d08a68661319b9365e7f25d337e595ca202cf2
freddyLeal <freddy.alejandro.leal@hotmail.com>
2015-11-24 20:44:18 -0500

    asdf

--- js/action.js
+++ js/action.js
@@ -17,15 +17,45 @@ function act(deltaTime) {
 		} else if (player.vx < 0) { 
 			player.vx += 1; 
 		} 
-		// Gravity 
+
+
+		// Gravity Jump
 		player.vy += 1; 
 		if (player.vy > 10) { 
 			player.vy = 10; 
-		} // Jump 
+		} 
 		if (onGround && lastPress === KEY_UP) { 
 			player.vy = -10; 
 		} 
+
+		// Move player 
 		player.x += player.vx; 
+		for (i = 0, l = wall.length; i < l; i += 1) { 
+			if (player.intersects(wall[i]) && (wall[i].type===1||wall[i].type===2)) { 
+				if (player.vx > 0) { 
+					player.right = wall[i].left; 
+				} else { 
+					player.left = wall[i].right; 
+				} 
+				player.vx = 0; 
+			} 
+		} 
+		onGround = false; 
+		player.y += player.vy; 
+		for (i = 0, l = wall.length; i < l; i += 1) { 
+			if (player.intersects(wall[i]) && (wall[i].type===1||wall[i].type===2)) { 
+				if (player.vy > 0) { 
+					player.bottom = wall[i].top; 
+					onGround = true; 
+				} else { 
+					player.top = wall[i].bottom; 
+				} 
+				player.vy = 0; 
+			} 
+		} 
+	
+
+
 		// player.y += player.vy; 
 		cam.focus(player.x, player.y); 
 	}
